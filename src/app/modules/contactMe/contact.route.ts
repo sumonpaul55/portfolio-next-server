fd4fa4dcf4a0f5eb5d5c7805ae8265829f5a81e0
sumonpaul55 <sumpaul100@gmail.com>
2025-01-28 00:18:37 +0600

    fix the validation

--- src/app/modules/contactMe/contact.route.ts
+++ src/app/modules/contactMe/contact.route.ts
@@ -2,8 +2,12 @@ import { Router } from "express";
 import validateRequest from "../../middleware/validateRequest";
 import { contactValidation } from "./contact.validation";
 import { contactController } from "./contact.controller";
+import authGaurd from "../../middleware/authGaurd";
+import { USER_ROLE } from "../../utils/user.utils";
 
 const router = Router()
-router.post("/contact-me", validateRequest(contactValidation.contactDatavalidation), contactController.addContactData)
+router.post("/contactme", validateRequest(contactValidation.contactDatavalidation), contactController.addContactData)
+
+router.get("/getmessage", authGaurd(USER_ROLE.ADMIN), contactController.getMessage);
 
 export const contactRouter = router
--- src/app/modules/contactMe/contact.validation.ts
+++ src/app/modules/contactMe/contact.validation.ts
@@ -1,10 +1,13 @@
 import { z } from "zod";
 
 const contactDatavalidation = z.object({
+  body: z.object({
     name: z.string().min(1, "Name is required"), // Name must be a non-empty string
     email: z.string().email("Invalid email address"), // Email must be a valid email address
     message: z.string().min(1, "Message is required"), // Message must be a non-empty string
-  });
+    phone: z.string().min(1, "Phone is required"), // Phone must be a non-empty string
+  })
+});
 
 
-  export const contactValidation = {contactDatavalidation}
+export const contactValidation = { contactDatavalidation }
