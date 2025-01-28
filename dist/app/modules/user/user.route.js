d85c1e475af560963b814c813e4ac91caa7e5bc0
sumonpaul55 <sumpaul100@gmail.com>
2025-01-28 19:57:56 +0600

    dist delted

--- dist/app/modules/user/user.route.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.userRoute = void 0;
-const express_1 = require("express");
-const user_controller_1 = require("./user.controller");
-const router = (0, express_1.Router)();
-router.get("/", user_controller_1.userController.getAllUser);
-exports.userRoute = router;
